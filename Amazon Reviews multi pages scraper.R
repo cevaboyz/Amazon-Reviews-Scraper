3b7cb96e8a75d8345d80d738b4ccf7e58e486d79
Andrew <74262186+cevaboyz@users.noreply.github.com>
2021-09-15 22:34:32 +0200

    Update Amazon Reviews multi pages scraper.R

--- Amazon Reviews multi pages scraper.R
+++ Amazon Reviews multi pages scraper.R
@@ -1,49 +1,40 @@
-install.packages("purr")
+install.packages("purrr")
 
 library(rvest)
 library(purrr)
 library(dplyr)
 library(openxlsx)
+library(tidyr)
 
 
-page_numbers <- 1:29 #modify to the number of total pages of the reviews
+page_numbers <- 1:100
 
-purrr::map_df(page_numbers, ~{ 
+purrr::map_df(page_numbers, ~ { 
   
-  url_reviews <- paste0("https://www.amazon.it/product-reviews/B07WTHVQZH/ref=cm_cr_arp_d_paging_btm_next_",.x,"?ie=UTF8&reviewerType=all_reviews&pageNumber=", .x) #change the link according to the product ASIN
+  url_reviews <- paste0("https://www.amazon.it/ask/questions/asin/B088P8JSSZ/",.x,"/ref=ask_ql_psf_ql_hza?isAnswered=true") #change the ASIN according to the product
   
-  doc <- read_html(url_reviews) # Assign results to `doc`
+  page <- read_html(url_reviews) # Assign results to `doc`
   
-  # Review Title
-  doc %>% 
-    html_nodes("[class='a-size-base a-link-normal review-title a-color-base review-title-content a-text-bold']") %>%
-    html_text() -> review_title
+  # Review Question
+  page %>% html_nodes("[class='a-declarative']") %>% html_text() -> q
   
-  # Review Text
-  doc %>% 
-    html_nodes("[class='a-size-base review-text review-text-content']") %>%
-    html_text() -> review_text
-  
-  # Number of stars in review
-  doc %>%
-    html_nodes("[data-hook='review-star-rating']") %>%
-    html_text() -> review_star
-  
-  # Return a tibble
-  data.frame(review_title,
-             review_text,
-             review_star,
-             page =.x) 
+  data.frame(q) }) -> question
 
-  }) -> result
+question <- question %>% rename( domanda = q)
 
-#remove left and right whitespace and write the file in a xlsx format
 
-cols_to_be_rectified <- names(result)[vapply(result, is.character, logical(1))]
+question <- question %>% drop_na()
 
-result[,cols_to_be_rectified] <- lapply(result[,cols_to_be_rectified], trimws)
-  
-result <- result %>% mutate(review_star = str_sub(review_star, 1,1))
+question <- distinct(question)
 
-write.xlsx(result, file = "reviews.xlsx")
+question <- data.frame(lapply(question, trimws), stringsAsFactors = FALSE)
 
+question <- distinct(question)
+
+question <- question %>% rename(domanda = question)
+
+question <- question %>% mutate(domanda = gsub("Visualizza tutte le", "", domanda))
+
+question <- question %>% mutate(domanda = gsub("risposte", "", domanda))
+
+question <- question %>% mutate(domanda = gsub("Domande e clienti", "", domanda))
