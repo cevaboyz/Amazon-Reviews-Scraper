fa9906bf86a3cc599de8273875f548d62f61285d
Andrew <74262186+cevaboyz@users.noreply.github.com>
2022-04-07 11:39:02 +0200

    Update and rename ASIN TO PRODUCT NAME.R to ASIN to product name.R
    added the function to get the ASIN product code from a prompt

--- ASIN to product name.R
+++ ASIN to product name.R
@@ -1,10 +1,9 @@
-
 # Install / Load relevant packages
 if(!"pacman" %in% installed.packages()[,"Package"]) install.packages("pacman")
 pacman::p_load(rvest, dplyr, tidyr, stringr)
 
 # ASIN product code
-prod_code <- "###" #insert Asin Code
+prod_code <- dlgInput("Insert ASIN", Sys.info()["ASIN"])$res #insert Asin Code
 
 url <- paste0("https://www.amazon.it/dp/", prod_code) #change the url to match the domain
 
