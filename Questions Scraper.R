d299a5b1a9fa305bfa43f0b04959093629e57054
Andrew <74262186+cevaboyz@users.noreply.github.com>
2022-04-07 12:11:18 +0200

    Update and rename Amazon Question Scraper.R to Questions Scraper.R
    updated the script to obtain the questions from the customers about a product

--- Questions Scraper.R
+++ Questions Scraper.R
@@ -1,19 +1,21 @@
-install.packages("purrr")
-install.packages("beepr")
+#setting up the environment
+if(!"pacman" %in% installed.packages()[,"Package"]) install.packages("pacman")
+pacman::p_load(rvest, dplyr, tidyr, stringr, purrr, openxlsx, beepr, svDialogs)
 
 
-library(rvest)
-library(purrr)
-library(dplyr)
-library(openxlsx)
-library(beepr)
+#insert Asin Code
+prod_code <- dlgInput("Insert ASIN", Sys.info()["ASIN"])$res
 
+#insert the number of pages that you want to scrape
+page_number <- dlgInput("Insert the number of page of questions that you would like to scrape", Sys.info()["Page Number"])$res
 
-page_numbers <- 1:2
+#obtain the range from the page inserted
+page_numbers <- 1:page_number
 
+#iterate the function to get the html page and then extract the nodes containing the questions 
 purrr::map_df(page_numbers, ~ { 
   
-  url_reviews <- paste0("https://www.amazon.it/ask/questions/asin/B07GDBLY4Q/",.x,"/ref=ask_ql_psf_ql_hza?isAnswered=true")
+  url_reviews <- paste0("https://www.amazon.it/ask/questions/asin/",prod_code,"/",.x,"/ref=ask_ql_psf_ql_hza?isAnswered=true")
   
   page <- read_html(url_reviews) # Assign results to `doc`
   
@@ -22,9 +24,13 @@ purrr::map_df(page_numbers, ~ {
   
   data.frame(q) }) -> question
 
+
+#cleaning and formatting the dataframe obtained from the scrape
+#
 question_repulisti <- question
 
-question_repulisti <- na.omit(question_repulisti) #remove NA rows
+#remove NA rows_remove na
+question_repulisti <- na.omit(question_repulisti)
 
 question_repulisti <- str_trim(question_repulisti$q, side = "both")
 
@@ -32,6 +38,7 @@ question_repulisti <- as.data.frame(question_repulisti)
 
 question_repulisti <- unique(question_repulisti)
 
+#remove the not uniques elements as the CTA copy and other amazon's side content
 question_repulisti <-
   question_repulisti[!grepl("Selezione delle preferenze",
                             question_repulisti$question_repulisti), ]
@@ -68,6 +75,15 @@ question_repulisti <-
 
 question_repulisti <- as.data.frame(question_repulisti)
 
-beep()
+#getting the name of the product from the ASIN
+#obtain the text in the node, remove "\n" from the text, and remove white space
+prod <- html_nodes(doc, "#productTitle") %>% 
+  html_text() %>% 
+  gsub("\n", "", .) %>% 
+  trimws()
 
+names(question_repulisti)[1] <- "Domande"
 
+write.xlsx(question_repulisti, file = paste0(prod, "_question.xlsx"))
+
+beepr::beep(sound = 17)
